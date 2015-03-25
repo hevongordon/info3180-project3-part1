6a683ed32eb6e8b9805b5544be6fe4b9927a3e87
Hevon <hevongordon@gmail.com>
2015-03-25 17:52:57 -0500

    final games commit

---
+++ app/static/js/resource.js
@@ -0,0 +1,26 @@
+//game resources
+game.resources = [
+  /**
+   * Graphics.
+   */
+  // our level tileset
+  {name: "area01_level_tiles",  type:"image", src: "/static/data/img/map/area01_level_tiles.png"},
+  {name: "gripe_run_right", type:"image", src: "/static/data/img/sprite/gripe_run_right.png"},
+
+  {name: "area01_bkg0", type:"image", src: "/static/data/img/area01_bkg0.png"},
+
+   {name: "area01_bkg1", type:"image", src: "/static/data/img/area01_bkg1.png"},
+
+    {name: "spinning_coin_gold", type:"image", src: "/static/data/img/sprite/spinning_coin_gold.png"},
+
+{name: "wheelie_right", type:"image", src: "/static/data/img/sprite/wheelie_right.png"},
+
+{name: "32x32_font", type:"image", src: "/static/data/img/font/32x32_font.png"},
+
+
+ 
+  /*
+   * Maps.
+   */
+  {name: "area01", type: "tmx", src: "/static/data/map/area01.tmx"}
+];
